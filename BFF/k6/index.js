976baf9ee342a3171f9833c3b05a89f4dacb53a6
Kako Ferrare <kakoferrare87@gmail.com>
2025-09-15 09:34:24 -0300

    metrics

--- BFF/k6/index.js
+++ BFF/k6/index.js
@@ -13,3 +13,13 @@ export default function () {
     http.get('http://localhost:5121/Emprestimo/1');
     sleep(1);
 }
+
+
+//increase(process_cpu_time_seconds_total{app="BFF",env="Development"}[$__rate_interval])
+
+//increase(requisicoes_total{app="BFF"}[$__rate_interval])
+
+//histogram_quantile(0.5, sum(rate(http_server_request_duration_seconds_bucket{app="BFF"}[$__rate_interval])) by (le))
+//histogram_quantile(0.95, sum(rate(http_server_request_duration_seconds_bucket{app="BFF"}[$__rate_interval])) by (le))
+//histogram_quantile(0.90, sum(rate(http_server_request_duration_seconds_bucket{app="BFF"}[$__rate_interval])) by (le))
+//rate(requisicoes_total{app="BFF",tipo="http_sucesso"}[$__rate_interval])
