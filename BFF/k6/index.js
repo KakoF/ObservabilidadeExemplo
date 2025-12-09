fd77e1031c53fcef67c4c841a61916e9152e758c
Marcos Ferrare <marcosferrare@bemol.com.br>
2025-12-09 14:08:49 -0300

    dashboard

--- BFF/k6/index.js
+++ BFF/k6/index.js
@@ -10,8 +10,8 @@ export const options = {
 };
 
 export default function () {
-    http.get('http://localhost:5121/Emprestimo/1');
-    //http.get('http://host.docker.internal:5084/Emprestimo/1');
+    //http.get('http://localhost:5121/Emprestimo/1');
+    http.get('http://host.docker.internal:5121/Emprestimo/1');
     sleep(1);
 }
 
