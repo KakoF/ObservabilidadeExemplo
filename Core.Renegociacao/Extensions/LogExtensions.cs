a193bbb7483cfa609b4bfb771f495e4e10663d0d
Marcos Ferrare <marcosferrare@bemol.com.br>
2025-12-09 10:46:35 -0300

    refactor: core renegociacao

---
+++ Core.Renegociacao/Extensions/LogExtensions.cs
@@ -0,0 +1,39 @@
+﻿using OpenTelemetry.Logs;
+using OpenTelemetry.Resources;
+
+namespace Core.Renegociacao.Extensions
+{
+    public static class LogExtensions
+    {
+        public static void AddLog(this WebApplicationBuilder builder)
+        {
+            var appName = "Core.Renegociacao";
+            var otelUrl = builder.Configuration["Clients:Otel"];
+
+            var resourceBuilder = ResourceBuilder.CreateDefault()
+            .AddService(appName)
+            .AddAttributes(new[]
+            {
+                new KeyValuePair<string, object>("app", appName),
+                new KeyValuePair<string, object>("env", builder.Environment.EnvironmentName),
+                new KeyValuePair<string, object>("host.name", Environment.MachineName)
+            });
+
+
+            builder.Logging.Configure(options =>
+            {
+                options.ActivityTrackingOptions = ActivityTrackingOptions.TraceId | ActivityTrackingOptions.SpanId;
+            });
+            builder.Logging.AddOpenTelemetry(logging =>
+            {
+                logging.IncludeFormattedMessage = true;
+                logging.IncludeScopes = true;
+                logging.SetResourceBuilder(resourceBuilder);
+                logging.AddOtlpExporter(options =>
+                {
+                    options.Endpoint = new Uri(otelUrl!);
+                });
+            });
+        }
+    }
+}
--- Core.Renegociacao/Program.cs
+++ Core.Renegociacao/Program.cs
@@ -1,12 +1,5 @@
-using Core.Renegociacao.Meters;
+using Core.Renegociacao.Extensions;
 using Core.Renegociacao.Middleware;
-using Core.Renegociacao.Services;
-using OpenTelemetry;
-using OpenTelemetry.Exporter;
-using OpenTelemetry.Logs;
-using OpenTelemetry.Metrics;
-using OpenTelemetry.Resources;
-using OpenTelemetry.Trace;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -17,71 +10,14 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddHttpClient();
-var otelUrl = builder.Configuration["Clients:Otel"];
-var resourceBuilder = ResourceBuilder.CreateDefault()
-	.AddService("Core.Renegociacao")
-	.AddAttributes(new[]
-	{
-		new KeyValuePair<string, object>("app", "Core.Renegociacao"),
-		new KeyValuePair<string, object>("env", builder.Environment.EnvironmentName),
-		new KeyValuePair<string, object>("host.name", Environment.MachineName)
-	});
 
-builder.Services.AddOpenTelemetry()
-	.ConfigureResource(resource => resource.AddService("Core.Renegociacao").AddAttributes(new[]
-	{
-		new KeyValuePair<string, object>("app", "Core.Renegociacao"),
-		new KeyValuePair<string, object>("env", builder.Environment.EnvironmentName),
-		new KeyValuePair<string, object>("host.name", Environment.MachineName)
-	}))
-	.WithTracing(tracing => tracing
-		.AddAspNetCoreInstrumentation()
-		//.AddHttpClientInstrumentation()
-		.SetResourceBuilder(resourceBuilder)
-		.AddOtlpExporter(opt =>
-		{
-			opt.Endpoint = new Uri(otelUrl);
-			opt.Protocol = OtlpExportProtocol.Grpc;
-		}))
-	.WithMetrics(metrics => metrics
-		.SetResourceBuilder(resourceBuilder)
-		.AddAspNetCoreInstrumentation()
-		.AddHttpClientInstrumentation()
-		.AddRuntimeInstrumentation()
-		.AddProcessInstrumentation()
-		.AddEventCountersInstrumentation(options =>
-		{
-			options.AddEventSources("Microsoft.AspNetCore.Hosting", "System.Net.Http");
-		})
-		.AddMeter("CoreRenegociacao.*")
-		.AddOtlpExporter(options =>
-		{
-			options.Endpoint = new Uri(otelUrl);
-		}));
-builder.Services.AddSingleton<MetricsHelper>();
-builder.Services.AddScoped<EmprestimoService>();
-
-// Configuração de Logs
-builder.Logging.Configure(options =>
-{
-    options.ActivityTrackingOptions = ActivityTrackingOptions.TraceId | ActivityTrackingOptions.SpanId;
-});
-builder.Logging.AddOpenTelemetry(logging =>
-{
-	logging.IncludeFormattedMessage = true;
-	logging.IncludeScopes = true;
-	logging.AddOtlpExporter(options =>
-	{
-		options.Endpoint = new Uri(otelUrl);
-	});
-});
+builder.AddOtel();
+builder.AddLog();
 
 var app = builder.Build();
 
-
 app.UseSwagger();
 app.UseSwaggerUI();
-
 
 app.UseHttpsRedirection();
 
